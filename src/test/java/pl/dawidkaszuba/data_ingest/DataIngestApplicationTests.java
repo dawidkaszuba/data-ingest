221c817d2808bffeb308b0d7bf77ca0f8a6fd5aa
dawidkaszuba <d.kaszuba89@gmail.com>
2025-07-29 20:55:43 +0200

    first commit

---
+++ src/test/java/pl/dawidkaszuba/data_ingest/DataIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dawidkaszuba.data_ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
